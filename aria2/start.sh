ad4116c503d416047b4e591ad4e4b01e764be6ad
xiaolanglang <2145773+xiaolanglanglang@users.noreply.github.com>
2020-10-01 22:35:38 +0800

    feat: Add initialization function.

--- aria2/start.sh
+++ aria2/start.sh
@@ -1,13 +1,28 @@
 #!/bin/sh
 
 token="";
-if [[ $ARIA2_TOKEN ]]
+if [[ ! -f "/aria2/conf/aria2.conf" ]]
 then
-    token=$ARIA2_TOKEN
-else
-    token=`date +%s | sha256sum | base64 | head -c 32 ; echo`
-    echo aria2 token is $token
+    echo conf not found, use default.
+    cp /aria2/bak/aria2.conf /aria2/conf/aria2.conf
+    if [[ $ARIA2_TOKEN ]]
+    then
+        token=$ARIA2_TOKEN
+    else
+        token=`date +%s | sha256sum | base64 | head -c 32 ; echo`
+        echo aria2 token is $token
+    fi
+    sed -i "s/rpc-secret=.*$/rpc-secret=$token/" /aria2/conf/aria2.conf
 fi
-sed -i "s/rpc-secret=.*$/rpc-secret=$token/" /aria2/conf/aria2.conf
+if [[ ! -f /aria2/data/aria2.session ]]
+then
+    echo session not found, use default.
+    cp /aria2/bak/aria2.session /aria2/data/aria2.session
+fi
+if [[ ! -f /aria2/data/dht.dat ]]
+then
+    echo dht.dat not found, create one.
+    touch /aria2/data/dht.dat
+fi
 
 /usr/bin/aria2c --conf-path=/aria2/conf/aria2.conf
