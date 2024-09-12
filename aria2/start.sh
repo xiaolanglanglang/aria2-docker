1c8ec76f84c2a94f2c1b9f4e76157e968c39eb1b
xiaolanglanglang <2145773+xiaolanglanglang@users.noreply.github.com>
2024-09-12 11:48:36 +0800

    feat: Update aria2 configuration files

--- aria2/start.sh
+++ aria2/start.sh
@@ -4,7 +4,7 @@ token="";
 if [[ ! -f "/aria2/conf/aria2.conf" ]]
 then
     echo conf not found, use default.
-    cp /aria2/bak/aria2.conf /aria2/conf/aria2.conf
+    cp /aria2/default/aria2.conf /aria2/conf/aria2.conf
     if [[ $ARIA2_TOKEN ]]
     then
         token=$ARIA2_TOKEN
@@ -17,12 +17,17 @@ fi
 if [[ ! -f /aria2/data/aria2.session ]]
 then
     echo session not found, use default.
-    cp /aria2/bak/aria2.session /aria2/data/aria2.session
+    cp /aria2/default/aria2.session /aria2/data/aria2.session
 fi
 if [[ ! -f /aria2/data/dht.dat ]]
 then
     echo dht.dat not found, create one.
     touch /aria2/data/dht.dat
+fi
+if [[ ! -f /aria2/data/dht6.dat ]]
+then
+    echo dht6.dat not found, create one.
+    touch /aria2/data/dht6.dat
 fi
 
 /usr/bin/aria2c --conf-path=/aria2/conf/aria2.conf
